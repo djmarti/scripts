4023e33240de70799097fbcc61ea84db9696d6f8
Daniel Marti <daniel.marti@ens.fr>
2016-05-27 16:15:02 +0200

    Terser, more pythonic, python3-ready

--- canonize_jnames.py
+++ canonize_jnames.py
@@ -1,66 +1,48 @@
 #!/usr/bin/env python3
 
-"""Given a BibTeX file (.bib) with with short and/or long journal names in it,
+'''Given a BibTeX file (.bib) with with short and/or long journal names in it,
 return a new .bib file with the journal names abbreviated (default) or
 complete, using the standard abbreviatures of the the Congress Library
 stored in a file called 'journal_abbreviations.txt'.
 
-TODO: It would be cool to fill 'journal_abbreviations.txt' automatically."""
+TODO: It would be cool to fill 'journal_abbreviations.txt' automatically.'''
 
 import re
 import os.path
 import sys
 import getopt
-from UserDict import UserDict
 
 try:
-    opts, args = getopt.gnu_getopt(sys.argv[1:], "sl", ["short", "long"])
+    opts, args = getopt.gnu_getopt(sys.argv[1:], 'sl', ['short', 'long'])
 except getopt.GetoptError:
     sys.exit(2)
 
 complete = True
 
 for o, v in opts:
-    if o in ("-s", "--short"):
+    if o in ('-s', '--short'):
         complete = False
-    if o in ("-l", "--long"):
+    if o in ('-l', '--long'):
         complete = True
-
 
 # Original algorithm by Xavier Defrang.
 # http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/81330
 # This implementation by alane@sourceforge.net.
+# Updated in
+# http://stackoverflow.com/questions/15175142/how-can-i-do-multiple-substitutions-using-regex-in-python
 
-class textsub(UserDict):
-    """This recipe shows how to use the Python standard re module to perform
-    single-pass multiple string substitution using a dictionary."""
-
-    def __init__(self, dict=None):
-        self.re = None
-        self.regex = None
-        UserDict.__init__(self, dict)
-
-    def compile(self):
-        if len(self.data) > 0:
-            tmp = r"(\b%s\b)" % "|".join(self.data.keys())
-            tmp = tmp.replace(r"\ ", r"\s*")
-            if self.re != tmp:
-                self.re = tmp
-                self.regex = re.compile(self.re)
-
-    def __call__(self, match):
-        return self.data[match.string[match.start():match.end()]]
-
-    def replace(self, s):
-        if len(self.data) == 0:
-            return s
-        return self.regex.sub(self, s)
+def multiple_replace(d, text):
+    '''This recipe shows how to use the Python standard re module to perform
+    single-pass multiple string substitution using a dictionary.'''
+    regex = re.compile('({})'.format('|'.join(map(re.escape, d.keys()))))
+    return regex.sub(
+        lambda mo: d[mo.string[mo.start():mo.end()]], text)
 
 jnames = {}
 f_input = os.path.join(os.path.dirname(__file__),
-                       "journal_abbreviations.txt")
+                       'journal_abbreviations.txt')
 
-fileIN = open(f_input, "r")
+fileIN = open(f_input, 'r')
 string = fileIN.read()
 fileIN.close()
 
@@ -79,15 +61,12 @@ for l in lines:
     jnames[long] = abbrv
 
 # Read bib file (with full, long journal names)
-b = open(args[0], "r")
+b = open(args[0], 'r')
 s = b.read()
 b.close()
 s = s.rstrip('\n')
 
 journal_pat = re.compile('(\s*journal\s*=\s*"(\\.|[^"])*)\n+\s*((\\.|[^"])*")')
-s = journal_pat.sub(r"\1 \3", s)
+s = journal_pat.sub(r'\1 \3', s)
 
-X = textsub(jnames)
-X.compile()
-o = X.replace(s)
-print(o)
+print(multiple_replace(jnames, s))
